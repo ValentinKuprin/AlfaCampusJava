b3a23948d787e8df02166bc77475ccd9c8d0df45
sergei <skozelkov@alfabank.ru>
2023-01-19 18:41:51 +0300

    add tests description

--- JavaHW2/src/test/java/ru/alfabank/campus/person/PersonTest.java
+++ JavaHW2/src/test/java/ru/alfabank/campus/person/PersonTest.java
@@ -3,7 +3,7 @@ package ru.alfabank.campus.person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
 
